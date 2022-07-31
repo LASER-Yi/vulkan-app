496757f9bd462a090fa54c22921afbf3b5c1f46e
LASER-Yi <liangyi0007@gmail.com>
2022-07-31 15:12:18 +0800

    Add basic vulkan codes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+
 #include <iostream>
-#include "renderer.h"
 
-int main()
-{
-    return 0;
+int main() {
+  glfwInit();
 
-    VulkanRenderer renderer;
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+  std::cout << extensionCount << " extensions supported\n";
+
+  glm::mat4 matrix;
+  glm::vec4 vec;
+  auto test = matrix * vec;
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
+
+  return 0;
 }
