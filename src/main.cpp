671a62e9f8a29194b231bef641603fdf2d2ae585
LASER-Yi <liangyi0007@gmail.com>
2022-08-01 12:52:56 +0800

    Improve CMake configurations

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 #include <cstdlib>
 #include <iostream>
 
-int main() {
-  GameEngine app;
-  try {
-    app.run();
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
-    return EXIT_FAILURE;
-  }
-  return EXIT_SUCCESS;
+int main()
+{
+    GameEngine app;
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
