4a76e0bee9808ab475fdcd8bb6a23e965d61b237
LASER-Yi <liangyi0007@gmail.com>
2022-08-01 22:47:30 +0800

    Add platform marcos support

--- engine/Private/VulkanRHI.cpp
+++ engine/Private/VulkanRHI.cpp
@@ -58,13 +58,13 @@ void GameEngine::createInstance()
                           validationLayers.end());
     }
 
-    // Workaround for VK_ERROR_INCOMPATIBLE_DRIVER
-    {
-        createInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
-        extensionNames.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
-        extensionNames.push_back(
-            VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
-    }
+// Workaround for VK_ERROR_INCOMPATIBLE_DRIVER
+#if WITH_MOLTEN_VK
+    createInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+    extensionNames.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+    extensionNames.push_back(
+        VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
+#endif
 
     createInfo.enabledExtensionCount = extensionNames.size();
     createInfo.ppEnabledExtensionNames = extensionNames.data();
