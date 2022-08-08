5d372a2dbbdda2e204236f7fd282609639d2df63
LASER-Yi <liangyi0007@gmail.com>
2022-08-09 07:57:33 +0800

    Handle vulkan result properly

--- src/Engine/Public/VulkanRHI/VulkanCommon.h
+++ src/Engine/Public/VulkanRHI/VulkanCommon.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <cstdint>
+
 #include <cstring>
 #include <format>
 #include <memory>
@@ -10,26 +10,33 @@
     {                                                                          \
         const vk::Result ScopedResult = VkFunction;                            \
         if (ScopedResult != vk::Result::eSuccess) {                            \
-            VerifyVulkanResult(ScopedResult, #VkFunction, __FILE__, __LINE__); \
+            _verifyVulkanResult(ScopedResult, #VkFunction, __FILE__,           \
+                                __LINE__);                                     \
         }                                                                      \
     }
 
 #define VERIFY_VULKAN_RESULT_VALUE(VkFunction)                                 \
     [](const auto& ResultValue) {                                              \
         if (ResultValue.result != vk::Result::eSuccess) {                      \
-            VerifyVulkanResult(ResultValue.result, #VkFunction, __FILE__,      \
-                               __LINE__);                                      \
+            _verifyVulkanResult(ResultValue.result, #VkFunction, __FILE__,     \
+                                __LINE__);                                     \
         }                                                                      \
         return ResultValue.value;                                              \
     }(VkFunction);
 
 template <typename T>
-static inline void VerifyVulkanResult(const T& Result, const char* VkFunction,
-                                      const char* Filename, uint32_t Line)
+static inline void _verifyVulkanResult(const T& Result, const char* VkFunction,
+                                       const char* Filename, uint32_t Line)
 {
 #if __cpp_lib_format
     throw std::runtime_error(
         std::format("%s failed, VkResult=%d\n at %s:%u \n", VkFunction,
                     static_cast<int32_t>(Result), Filename, Line));
+#else
+    std::string errorMsg = "%s failed, VkResult=%d\n at %s:%u \n";
+    sprintf(errorMsg.data(), VkFunction, static_cast<int32_t>(Result), Filename,
+            Line);
+
+    throw std::runtime_error(errorMsg);
 #endif
 }
